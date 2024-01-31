20f18b89082b87b660b528ef9f5669ac36909207
Jimcel4 <155282317+Jimcel4@users.noreply.github.com>
2024-01-31 20:54:52 +0800

    Update register.php

--- register.php
+++ register.php
@@ -4,14 +4,6 @@ if(isset($_SESSION['logged_in'])){
     ob_end_flush();
 }
 ?>
-<style>
-    body {
-        background-image: url("img/kh.jpg");
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        background-attachment: fixed;
-    }
-</style>
 
 <div class="container">
     <div class="row justify-content-center">
@@ -23,43 +15,42 @@ if(isset($_SESSION['logged_in'])){
                 </div>
             <?php } ?>
             <form method="POST" action="process.php">
-            <center><p><h5>Register</h5></p></center>
                 <div class="row mb-3">
-                    <label for="fname" class="col-sm-4 col-form-label"><b>First Name</b></label>
+                    <label for="fname" class="col-sm-4 col-form-label">First Name</label>
                     <div class="col-sm-8">
                         <input type="text" class="form-control" id="fname" name="f_name" required>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="lname" class="col-sm-4 col-form-label"><b>Last Name</b></label>
+                    <label for="lname" class="col-sm-4 col-form-label">Last Name</label>
                     <div class="col-sm-8">
                         <input type="text" class="form-control" id="lname" name="l_name" required>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="email" class="col-sm-4 col-form-label"><b>Email</b></label>
+                    <label for="email" class="col-sm-4 col-form-label">Email</label>
                     <div class="col-sm-8">
                         <input type="email" class="form-control" id="email" name="email" required>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="password" class="col-sm-4 col-form-label"><b>Password</b></label>
+                    <label for="password" class="col-sm-4 col-form-label">Password</label>
                     <div class="col-sm-8">
                         <input type="password" class="form-control" id="password" name="password1" required>
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="pass2" class="col-sm-4 col-form-label"><b>Confirm Password</b></label>
+                    <label for="pass2" class="col-sm-4 col-form-label">Confirm Password</label>
                     <div class="col-sm-8">
                         <input type="password" class="form-control" id="pass2" name="password2" required>
                     </div>
-                </div> 
-             <center>
-                <button type="submit" class="btn btn-primary" name="register"><b>Sign up</b></button></center>
+                </div>
+                <button type="submit" class="btn btn-primary" name="register">Sign up</button>
             </form>
         </div>
     </div>
 </div>
+<!-- content end -->
 </body>
 
 </html>
